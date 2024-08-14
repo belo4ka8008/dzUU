71bfcbeb2723c1aa1ad37190f8b6afb92606e440
belo4ka8008 <dmitry_bessmertnyy@mail.ru>
2024-08-14 21:23:42 +0300

    Add files via upload
    ДЗ "переменные"

---
+++ peremennie.py
@@ -0,0 +1,6 @@
+amount = 12
+hours = 1.5
+name = "Python"
+one_task = (hours/amount)
+
+print("Курс: "+name+", всего задач: "+str(amount)+", затрачено часов: "+str(hours)+", среднее время выполнения: "+str(one_task)+" часа.")
