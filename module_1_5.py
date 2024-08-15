485207dbea1f2b0be157a95408e590893c006786
belo4ka8008 <dmitry_bessmertnyy@mail.ru>
2024-08-15 21:23:15 +0300

    Add files via upload
    Неизменяемые и изменяемые объекты. Кортежи

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 1,2,'3',4, True
+print(immutable_var)
+#immutable_var[2] = 3
+#print(immutable_var)
+#кортеж - неизменяемая коллекция элементов
+
+mutable_list = [1,2,'3',4, True]
+mutable_list[4] = 'abc'
+print(mutable_list)
