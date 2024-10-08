a624d61982c1237bd81267ea771102f28f6002aa
belo4ka8008 <dmitry_bessmertnyy@mail.ru>
2024-10-08 22:13:20 +0300

    Задача "Мифическое наследование"

---
+++ module_6_3.py
@@ -0,0 +1,33 @@
+#Задача "Мифическое наследование"
+
+class Horse:
+    x_distance = 0
+    sound = 'Frrr'
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    y_distance = 0
+    _sound = 'I train, sleep, and repeat'
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+    def voice(self):
+        print(self._sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
