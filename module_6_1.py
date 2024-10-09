b36e9b7e2abfc2e501083a01d9188db100254437
belo4ka8008 <dmitry_bessmertnyy@mail.ru>
2024-10-09 15:44:32 +0300

    #Задача "Съедобное, несъедобное"
    eat inside animal

--- module_6_1.py
+++ module_6_1.py
@@ -5,7 +5,13 @@ class Animal:
         fed = False
         def __init__(self, name):
             self.name = name
-
+        def eat(self, food):
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
 
 class Plant:
         edible = False
@@ -14,25 +20,13 @@ class Plant:
 
 
 class Mammal(Animal):
+    def __init__(self, name):
+        self.name = name
 
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
 
 class Predator(Animal):
     def __init__(self, name):
         self.name = name
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
 
 class Flower(Plant):
         edible = False
